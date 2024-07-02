247c92023765e2c80798aa4b70c446a8f32be0cf
Anshika Singh <anshikasingh02102004@gmail.com>
2024-07-02 06:30:29 -0700

    #4 loops, for loop,range,iterable,break and continue

---
+++ pattern1.py
@@ -0,0 +1,45 @@
+from turtle import *
+# square
+fd(120)
+rt(60)
+fd(120)
+rt(60)
+fd(120)
+rt(60)
+fd(120)
+rt(60)
+fd(120)
+rt(60)
+fd(120)
+rt(60)
+mainloop()
+# decagon
+fd(120)
+rt(36)
+fd(120)
+rt(36)
+fd(120)
+rt(36)
+fd(120)
+rt(36)
+fd(120)
+rt(36)
+fd(120)
+rt(36)
+fd(120)
+rt(36)
+fd(120)
+rt(36)
+fd(120)
+rt(36)
+fd(120)
+rt(36)
+mainloop()
+from turtle import *
+# for loop
+side = 8
+for i in range(side):
+      fd(120)
+      lt(360/side)
+mainloop()      
+
